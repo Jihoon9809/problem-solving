80ca2503dfb810d6c8138a0c11ec5b251552da10
Jihoon9809 <aaa@gmail.com>
2023-03-16 00:16:12 +0900

    0316_DP

--- python/step2(DP)/2193.py
+++ python/step2(DP)/2193.py
@@ -1,13 +1,14 @@
 n = int(input())
 
-arr = [[0 for i in range(n+1)]for i in range(2)]
+arr = [[0 for i in range(2)]for i in range(n+1)]
 
-arr[1][0] = 0
 arr[1][1] = 1
 
-arr[2][0] = 1
 arr[2][1] = 1
 
-asdf
-
+for i in range(3,n+1):
+    for j in range(2):
+        arr[i][j] = arr[i-1][j] + arr[i-2][j]
+        
+print(arr[n][0] + arr[n][1])
 
