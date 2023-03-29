03fa44a075960bdffec786283448e4c3cd20d7db
Jihoon9809 <dkdb521@naver.com>
2023-03-29 22:31:48 +0900

    0329.py

---
+++ python/step2(DP)/1699.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = [i for i in range(n+1)]
+
+for i in range(2,n+1):
+    target = i ** 2
+    
+    if len(arr) <= target :
+        break
+    
+    arr[target] = 1
+    
+    for j in range(target+1,n+1):
+        arr[j] = arr[j - target] + arr[target]
+        
+print(arr[n])
